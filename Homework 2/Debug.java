55606eb8ff3e94b5e613a8294d65f4476b5fd0e7
batitto <williambatitto@gmail.com>
2024-09-15 22:58:31 -0400

    donee

--- Homework 2/Debug.java
+++ Homework 2/Debug.java
@@ -1,5 +1,5 @@
 
-public class Main {
+public class Debug {
     public static void main(String[] args) {
 
         //for this section: are these all printing the best option? If they aren't, fix it!
@@ -7,20 +7,24 @@ public class Main {
         int var1 = 4;
         if (var1 > 4){
             System.out.println("Var1 is greater than 4");
+        } else if(var1 == 4){
+            System.out.println("Var1 is equal to 4");
         }else{
             System.out.println("Var1 is less than 4");
         }
+        //i added an else if case to cover if var1 is equal to 4
 
         int var2 = 6;
         if (var2 == 5){
             System.out.println("Var2 is 5");
-        } else if (var2 > 5){
-            System.out.println("Var2 is greater than 5");
+        } else if (var2 == 6){
+            System.out.println("Var2 is 6");
         } else if (var2 < 5){
             System.out.println("Var2 is less than 5");
         } else{
-            System.out.println("Var2 is 6");
+            System.out.println("Var2 is greater than 5");
         }
+        //i swapped around the first else if with one that checks if var2 equals 6 since the else statement cant check that 
 
         int var3 = 5;
         if (var3 > 10){
@@ -28,17 +32,20 @@ public class Main {
         } else {
             System.out.println("Var3 is less than 10");
         }
-
+        //if we are just checking if var3 is greater than or less than 10 then it is giving the best answer 
 
 
 
         //for this section: why are we not entering the if statement?
-        if ("Marist" == "marist"){
+
+        //the if statement does not check if a variable equals "marist"
+        String Marist = "marist";
+        if (Marist == "marist"){
             System.out.println("Marist college!");
         } else{
             System.out.println("Not marist college :(!");
         }
-
+        //i added a varible called Marist and set it equal to "marist" and made the if statement check the variable 
 
     }
 }
--- Homework 2/ifelsepractice.java
+++ Homework 2/ifelsepractice.java
@@ -8,11 +8,45 @@ import java.util.Scanner;
 * can be any topic you pick :) feel free to pick some obscure or niche topics!
 * good luck!
 * */
-public class Main {
+public class ifelsepractice {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int score = 0;
 
+        System.out.println("Welcome to my Final Fantasy XIV quiz!");
 
+        System.out.println("Q1) What is Dark Knight tank stance called?");
+            String A1 = sc.nextLine();
+            if (A1.equalsIgnoreCase("Grit")){
+                System.out.println("Correct!");
+                score = score + 1;
+            } else{
+                System.out.println("Incorrect. The answer was Grit");
+            }
+
+        System.out.println("Q2) What is the name of the alliance raid series in A Realm Reborn?");
+            String A2 = sc.nextLine();
+            if(A2.equalsIgnoreCase("Crystal Tower")){
+                System.out.println("Correct");
+                score = score + 1;
+            } else{
+                System.out.println("Incorrect. The answer was Crystal Tower");
+            }
+
+        System.out.println("Q3) What is the only class that correlates to two seprate jobs?");
+            String A3 = sc.nextLine();
+            if (A3.equalsIgnoreCase("Arcanist")){
+                System.out.println("Correct!");
+                score = score + 1;
+            } else{
+                System.out.println("Incorrect. The answer was Arcanist");
+            }
+        
+        System.out.println("Great job!");
+        System.out.println("Your score was: " + score +"!");
+
+        sc.close(); 
     }
 }
 
